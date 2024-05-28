5be1d0d642f07ec69b891dacd2e4e2019b865360
Naitsyrk95 <kry.zawadzki2203@gmail.com>
2024-05-28 12:27:10 +0200

    my third commit

--- src/main/java/com/example/parkingserviceapi/time/TimeWatch.java
+++ src/main/java/com/example/parkingserviceapi/time/TimeWatch.java
@@ -1,30 +1,40 @@
 package com.example.parkingserviceapi.time;
 
-import java.util.concurrent.TimeUnit;
+
 
 public class TimeWatch {
-    long starts;
+    private long startTime = 0;
+    private long stopTime = 0;
+    private boolean running = false;
 
-    public static TimeWatch start() {
-        return new TimeWatch();
-    }
 
-    private TimeWatch() {
-        reset();
-    }
-
-    public TimeWatch reset() {
-        starts = System.currentTimeMillis();
-        return this;
-    }
-
-    public long time() {
-        long ends = System.currentTimeMillis();
-        return ends - starts;
+    public void start() {
+        this.startTime = System.currentTimeMillis();
+        this.running = true;
+        System.out.println("The time has been started");
     }
 
 
-    public long time(TimeUnit unit) {
-        return unit.convert(time(), TimeUnit.MILLISECONDS);
+    public void stop() {
+        this.stopTime = System.currentTimeMillis();
+        this.running = false;
+        System.out.println("Parking time has been stopped");
+    }
+
+    public long getElapsedTimeSecs() {
+        long elapsed;
+        if (running) {
+            elapsed = ((System.currentTimeMillis() - startTime) / 1000);
+        } else {
+            elapsed = ((stopTime - startTime) / 1000);
+        }
+        return elapsed;
+    }
+
+    public static void main(String[] args) {
+        TimeWatch tw = new TimeWatch();
+        tw.start();
+        tw.stop();
+        tw.getElapsedTimeSecs();
     }
 }
