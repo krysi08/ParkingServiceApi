9fe40961c7aed68ef1ccb1243353edc2d00be1dc
Naitsyrk95 <kry.zawadzki2203@gmail.com>
2024-05-29 19:03:56 +0200

    my fifth commit

--- src/main/java/com/example/parkingserviceapi/fee/PriceList.java
+++ src/main/java/com/example/parkingserviceapi/fee/PriceList.java
@@ -1,26 +1,27 @@
 package com.example.parkingserviceapi.fee;
 
-public class PriceList {
+import com.example.parkingserviceapi.time.TimeWatch;
+
+import java.util.Timer;
+
+public class PriceList  {
 
     public static void main(String[] args) {
 
         PriceList list = new PriceList();
-
-        // list.settleCostsParking(96);
     }
 
     public void settleCostsParking(double timeParking) {
-        if (timeParking >= 10.00 && timeParking <= 60.00) {
+        if (timeParking >= 10 && timeParking <= 60) {
             System.out.println("Please pay off 5EUR");
-        } else if (timeParking > 60.00 && timeParking <= 240.00) {
-            double valueCosts = timeParking * 5 / 60.00;
+        } else if (timeParking > 60 && timeParking <= 240) {
+            double valueCosts = timeParking * 5 / 60;
             System.out.println("You have to pay off" + " " + valueCosts + " " + "EUR");
-        } else if (timeParking > 240.00) {
+        } else if (timeParking > 240) {
             double discountCosts = (timeParking * 4) / 60;
             System.out.println("You have to pay off" + " " + discountCosts + " " + "EUR");
         } else {
             System.out.println("No fees");
         }
     }
-    double howlong;
 }
