5251132aaa75793a1cbde1479d00e7b8c6a5fa2e
Naitsyrk95 <kry.zawadzki2203@gmail.com>
2024-06-11 17:33:33 +0200

    my next commit

--- src/test/java/com/example/parkingserviceapi/PriceListTest.java
+++ src/test/java/com/example/parkingserviceapi/PriceListTest.java
@@ -3,10 +3,8 @@ package com.example.parkingserviceapi;
 import com.example.parkingserviceapi.fee.PriceList;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assumptions.assumingThat;
+
 
 @SpringBootTest
 public class PriceListTest {
@@ -18,76 +16,33 @@ public class PriceListTest {
     }
 
     @Test
-    public void settle_parking_cost_the_cheapest_one() {
-        String requiredFee = "Please pay off 5EUR";
+    public void settle_parking_cost_till_10_minutes() {
 
-        priceList.settleCostsParking(10);
-        assumingThat(
-                requiredFee.equals("Please pay off 5EUR"),
-                () -> assertEquals(10, 10)
-        );
+        double calculatedFee = priceList.settleCostsParking(5);
+
+        assertEquals(0, calculatedFee);
     }
 
     @Test
-    public void settle_parking_cost() {
-        String requiredFee1 = "Please pay off 5EUR";
+    public void settle_parking_cost_till_one_hour() {
 
-        priceList.settleCostsParking(60);
-        assumingThat(
-                requiredFee1.equals("Please pay off 5EUR"),
-                () -> assertEquals(60, 60)
-        );
+        double calculatedFee = priceList.settleCostsParking(50);
+
+        assertEquals(5, calculatedFee);
+    }
+    @Test
+    public void settle_parking_cost_till_four_hour() {
+
+        double calculatedFee = priceList.settleCostsParking(180);
+
+        assertEquals(15, calculatedFee);
     }
 
     @Test
-    public void settle_parking_cost_medium() {
-        String requiredFee2 = "You have to pay off" + " " + " " + "EUR";
+    public void settle_parking_cost_over_four_hour() {
 
-        priceList.settleCostsParking(61);
-        assumingThat(
-                requiredFee2.equals("You have to pay off" + " " + " " + "EUR"),
-                () -> assertEquals(61, 61)
-        );
-    }
+        double calculatedFee = priceList.settleCostsParking(300);
 
-    @Test
-    public void settle_parking_cost_medium_grather() {
-        String requiredFee3 = "You have to pay off" + " " + " " + "EUR";
-
-        priceList.settleCostsParking(240);
-        assumingThat(
-                requiredFee3.equals("You have to pay off" + " " + " " + "EUR"),
-                () -> assertEquals(240, 240)
-        );
-    }
-
-    @Test
-    public void settle_parking_cost_discount() {
-        String requiredFee4 = "You have to pay off" + " " + " " + "EUR";
-
-        priceList.settleCostsParking(241);
-        assumingThat(
-                requiredFee4.equals("You have to pay off" + " " + " " + "EUR"),
-                () -> assertEquals(241, 241)
-        );
-    }
-
-    @Test
-    public void settle_parking_cost_no_fees() {
-        String requiredFee5 = "No fees";
-
-        priceList.settleCostsParking(5);
-        assumingThat(
-                requiredFee5.equals("No fees"),
-                () -> assertEquals(5, 5)
-        );
-    }
-    @Test
-    void assertThrowsException() {
-        String requiredFee6 = null;
-        assertThrows(IllegalArgumentException.class, () -> {
-                    Integer.valueOf(requiredFee6);
-                }
-        );
+        assertEquals(20, calculatedFee);
     }
 }
